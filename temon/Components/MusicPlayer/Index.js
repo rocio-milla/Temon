d999daad4a341fb05915558708b60932354fbec4
Leandro David Coronel <coronelleandro18@gmail.com>
2020-10-09 05:46:34 -1200

    rama develop

---
+++ temon/Components/MusicPlayer/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Image,Text, View} from 'react-native';
+import { Button, Icon, } from 'react-native-elements';
+
+const MusicPlayerScreen = () => {
+    return (
+        <>
+            <View>Hola</View>
+        </>
+    );
+}
+
+export default MusicPlayerScreen;
