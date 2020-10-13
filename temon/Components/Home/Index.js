5435bd67405ab6cded5c2c9b60554ff37b027768
mquiroz25 <mquiroz2535@gmail.com>
2020-10-12 21:03:46 -0300

    captura de audio

--- temon/Components/Home/Index.js
+++ temon/Components/Home/Index.js
@@ -3,10 +3,44 @@ import {
     Image,
     Text, View
 } from 'react-native';
-import { Button, Icon, } from 'react-native-elements';
+import { Button, Icon} from 'react-native-elements';
+import LiveAudioStream from 'react-native-live-audio-stream';
+import { Buffer } from 'buffer';
 
 const HomeScreen = ({ navigation }) => {
+
+  iniciarGrabacion = () => {
+
+const options = {
+    sampleRate: 32000,  // default is 44100 but 32000 is adequate for accurate voice recognition
+    channels: 1,        // 1 or 2, default 1
+    bitsPerSample: 16,  // 8 or 16, default 16
+    audioSource: 6,     // android only (see below)
+  };
+
+  LiveAudioStream.init(options);
+
+    console.warn("iniciar")
+    
+    LiveAudioStream.start();
+
+   LiveAudioStream.on('data', data => {
+        var chunk = Buffer.from(data, 'base64');
+
+        console.warn(chunk);
+      });
+
+}
+
+detenerGrabacion = () => {  
+
+    console.warn("parar")
+    LiveAudioStream.stop();
+
+}
+
     return (
+
         <>
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Image source={require('../../images/index.jpg')}
@@ -17,9 +51,16 @@ const HomeScreen = ({ navigation }) => {
                     name="microphone"
                     type='font-awesome'
                     color='#a646dd'
-                    size={150}
+                    size={75}
                     reverse
-                    onPress={() => navigation.navigate('Results')}
+                    onPress={this.iniciarGrabacion} 
+                />
+                <Icon
+                    name="stop-circle"
+                    type='font-awesome'
+                    color='#a646dd'
+                    size={150}
+                    onPress={this.detenerGrabacion} 
                 />
             </View>
         </>
