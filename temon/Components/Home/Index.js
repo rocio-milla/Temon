98436527909a1763a203e6f7326f292c3c5035d6
rocio-milla <rmilla@alumno.unlam.edu.ar>
2020-10-15 22:49:42 -0300

    Fixed fetch

--- temon/Components/Home/Index.js
+++ temon/Components/Home/Index.js
@@ -1,56 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Image,
-    Text, View,TouchableOpacity
+    TouchableOpacity, View
 } from 'react-native';
-import { Button, Icon} from 'react-native-elements';
+import { Icon } from 'react-native-elements';
 import LiveAudioStream from 'react-native-live-audio-stream';
 import { Buffer } from 'buffer';
 
+const startRecording = (setValue) => {
+    console.warn("started")
+
+    LiveAudioStream.start();
+
+    LiveAudioStream.on('data', data => {
+        var chunk = Buffer.from(data, 'base64');
+        setValue(chunk)
+    });
+}
+
+const stopRecording = (value, setResultFetch) => {
+    LiveAudioStream.stop();
+    console.log("stopped! data: ", value);
+    fetch('https://77zrf57v5k.execute-api.us-east-1.amazonaws.com/default/solante-transcribe', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'x-api-key': 'taGJN51Ylz8PHK6Sfp96J2BUCnCL02rK5IlWvd2N',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: value
+    })
+        .then(response => { return response.json() })
+        .then(data => setResultFetch(data))
+        .catch(error => {
+            setResultFetch(null)
+            console.error(error)
+        });
+}
+
 const HomeScreen = ({ navigation }) => {
 
-    let value ;
-
+    const [value, setValue] = useState();
+    const [resultFetch, setResultFetch] = useState(null);
     const options = {
         sampleRate: 32000,  // default is 44100 but 32000 is adequate for accurate voice recognition
         channels: 1,        // 1 or 2, default 1
         bitsPerSample: 16,  // 8 or 16, default 16
         audioSource: 6,     // android only (see below)
-      };
-    
-      LiveAudioStream.init(options);
+    };
 
+    useEffect(() => {
+        LiveAudioStream.init(options);
+    }, [options])
 
-    startRecording = () => {
+    useEffect(() => {
+        if (resultFetch !== null) {
+            //Si resultFetch cambió y tiene algo dentro, hago lo que corresponda.
+            console.log(`Resultado del fetch: ${resultFetch}`)
+        }
 
-    console.warn("start")
-    
-    LiveAudioStream.start();
+        return () => {
+            setResultFetch(null)
+        }
+    }, [resultFetch])
 
-    LiveAudioStream.on('data', data => {
-      //  var chunk = Buffer.from(data, 'base64');
-        value = data;
-        console.warn(value);
-      });
-
-}
-
-stopRecording = () => {  
-
-    console.warn("stop")
-    LiveAudioStream.stop();
-
-    fetch('https://77zrf57v5k.execute-api.us-east-1.amazonaws.com/default/solante-transcribe', {
-        method: 'POST', // or 'PUT'
-        headers: {
-         'x-api-key': 'taGJN51Ylz8PHK6Sfp96J2BUCnCL02rK5IlWvd2N'
-        },
-        body: value
-      })
-  .then(response => response.json())
-  .then(data => console.warn(data))
-  .catch(error => console.error(error));
-}
     return (
 
         <>
@@ -59,17 +72,17 @@ stopRecording = () => {
                     style={{ height: "100%", width: "100%", resizeMode: 'contain' }} />
             </View>
             <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
-               
-             <TouchableOpacity
-              onPressIn={this.startRecording}    onPressOut={this.stopRecording}>
-                <Icon
-                    name="microphone"
-                    type='font-awesome'
-                    color='#a646dd'
-                    size={150}
-                    reverse
-                />
-             </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPressIn={() => startRecording(setValue)} onPressOut={() => stopRecording(value, setResultFetch)}>
+                    <Icon
+                        name="microphone"
+                        type='font-awesome'
+                        color='#a646dd'
+                        size={150}
+                        reverse
+                    />
+                </TouchableOpacity>
             </View>
         </>
     );
