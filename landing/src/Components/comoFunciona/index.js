97e660422df8537dad88ba045cf7eac463ac0016
rocio-milla <rmilla@alumno.unlam.edu.ar>
2020-10-17 21:16:48 -0300

    Aligned to left

--- landing/src/Components/comoFunciona/index.js
+++ landing/src/Components/comoFunciona/index.js
@@ -1,26 +1,25 @@
 import { Grid } from '@material-ui/core';
-import { Favorite, FiberManualRecord, MusicNote, PhoneAndroidRounded } from '@material-ui/icons';
 import React from 'react';
 
 const HowItWorks = () => {
     return (
         <div id="comofunciona">
-            <Grid container style={{ backgroundColor: "white", width: "100%" }}>
+            <Grid container style={{ backgroundColor: "white", width: "100%", margin: "1vh" }}>
                 <Grid item xs={12} md={12}>
-                    <h1 style={{ color: "black", fontWeight: 700, fontSize: "250%", marginBottom: 0 }}>¿QUÉ ES TEMON?</h1>
+                    <h1 style={{ color: "black", fontWeight: 700, fontSize: "250%", marginBottom: 0, textAlign: "left" }}>¿QUÉ ES TEMON?</h1>
                 </Grid>
 
                 <Grid item xs={12} md={12}>
-                    <h1 style={{ color: "black", fontWeight: 700, fontSize: "250%", marginBottom: 0 }}>TEMON ES UN REPRODUCTOR DE MÚSICA PARA USUARIOS DE BAJA VISIÓN.</h1>
-                    <FiberManualRecord color="primary" style={{ fontSize: "500%" }} />
+                    <h1 style={{ color: "black", fontWeight: 700, fontSize: "250%", marginBottom: 0, textAlign: "left"  }}>TEMON ES UN REPRODUCTOR DE MÚSICA PARA USUARIOS DE BAJA VISIÓN.</h1>
+                    {/* <FiberManualRecord color="primary" style={{ fontSize: "500%", textAlign: "center"  }} /> */}
                 </Grid>
                 <Grid item xs={12} md={12}>
-                    <h1 style={{ color: "black", fontWeight: 700, fontSize: "250%", marginBottom: 0 }}>CUENTA CON CONTRASTE ALTO, BOTONES GRANDES, BÚSQUEDA POR VOZ Y TODO AQUELLO QUE NO PODÉS HACER CON OTRAS APLICACIONES.</h1>
-                    <FiberManualRecord color="primary" style={{ fontSize: "500%" }} />
+                    <h1 style={{ color: "black", fontWeight: 700, fontSize: "250%", marginBottom: 0, textAlign: "left"  }}>CUENTA CON CONTRASTE ALTO, BOTONES GRANDES, BÚSQUEDA POR VOZ Y TODO AQUELLO QUE NO PODÉS HACER CON OTRAS APLICACIONES.</h1>
+                    {/* <FiberManualRecord color="primary" style={{ fontSize: "500%", textAlign: "center" }} /> */}
                 </Grid>
                 <Grid item xs={12} md={12}>
-                    <h1 style={{ color: "black", fontWeight: 700, fontSize: "250%", marginBottom: 0 }}>TODOS ESCUCHAMOS MÚSICA, ¿O NO?</h1>
-                    <FiberManualRecord color="primary" style={{ fontSize: "500%", marginBottom: "2%" }} />
+                    <h1 style={{ color: "black", fontWeight: 700, fontSize: "250%", marginBottom: 0, textAlign: "left"  }}>TODOS ESCUCHAMOS MÚSICA, ¿O NO?</h1>
+                    {/* <FiberManualRecord color="primary" style={{ fontSize: "500%", marginBottom: "2%", textAlign: "center" }} /> */}
                 </Grid>
             </Grid>
         </div>
