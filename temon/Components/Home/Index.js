9fc7fe2ce10d7cb3b9c0fcf33c2cf275bebd3b04
Juan Milla <juan.milla@southworks.com>
2020-10-19 22:49:39 -0300

    se agrega voz-a-texto

--- temon/Components/Home/Index.js
+++ temon/Components/Home/Index.js
@@ -7,62 +7,121 @@ import { Icon } from 'react-native-elements';
 import LiveAudioStream from 'react-native-live-audio-stream';
 import { Buffer } from 'buffer';
 
-const startRecording = (setValue) => {
-
-    LiveAudioStream.start();
-
-    LiveAudioStream.on('data', data => {
-        // var chunk = Buffer.from(data, 'base64');
-        setValue(data)
-    });
-}
-
-const stopRecording = (value, setResultFetch) => {
-    console.log(value.toString())
-    LiveAudioStream.stop();
-    fetch('https://77zrf57v5k.execute-api.us-east-1.amazonaws.com/default/solante-transcribe', {
-        method: 'POST', // or 'PUT'
-        headers: {
-            'x-api-key': 'taGJN51Ylz8PHK6Sfp96J2BUCnCL02rK5IlWvd2N',
-            // 'Accept': 'application/json',
-            // 'Content-Type': 'application/json'
-            "Content-Type": "application/octet-stream"
-        },
-        body: value
-    })
-        .then(response => { return response.json() })
-        .then(data => setResultFetch(data))
-        .catch(error => {
-            setResultFetch(null)
-            console.error(error)
-        });
-}
+// import Voice
+import Voice from '@react-native-community/voice';
 
 const HomeScreen = ({ navigation }) => {
 
-    const [value, setValue] = useState();
-    const [resultFetch, setResultFetch] = useState(null);
-    const options = {
-        sampleRate: 32000,  // default is 44100 but 32000 is adequate for accurate voice recognition
-        channels: 1,        // 1 or 2, default 1
-        bitsPerSample: 16,  // 8 or 16, default 16
-        audioSource: 6,     // android only (see below)
+  const [pitch, setPitch] = useState('');
+    const [error, setError] = useState('');
+    const [end, setEnd] = useState('');
+    const [started, setStarted] = useState('');
+    const [results, setResults] = useState([]);
+    const [partialResults, setPartialResults] = useState([]);
+
+    useEffect(() => {
+      //Setting callbacks for the process status
+      Voice.onSpeechStart = onSpeechStart;
+      Voice.onSpeechEnd = onSpeechEnd;
+      Voice.onSpeechError = onSpeechError;
+      Voice.onSpeechResults = onSpeechResults;
+      Voice.onSpeechPartialResults = onSpeechPartialResults;
+      Voice.onSpeechVolumeChanged = onSpeechVolumeChanged;
+
+      return () => {
+        //destroy the process after switching the screen
+        Voice.destroy().then(Voice.removeAllListeners);
+      };
+    }, []);
+
+    const onSpeechStart = (e) => {
+      //Invoked when .start() is called without error
+      console.log('onSpeechStart: ', e);
+      setStarted('√');
     };
 
-    useEffect(() => {
-        LiveAudioStream.init(options);
-    }, [options])
+    const onSpeechEnd = (e) => {
+      //Invoked when SpeechRecognizer stops recognition
+      console.log('onSpeechEnd: ', e);
+      setEnd('√');
+    };
 
-    useEffect(() => {
-        if (resultFetch !== null) {
-            //Si resultFetch cambió y tiene algo dentro, hago lo que corresponda.
-            console.log(`Resultado del fetch: ${resultFetch}`)
-        }
+    const onSpeechError = (e) => {
+      //Invoked when an error occurs.
+      console.log('onSpeechError: ', e);
+      setError(JSON.stringify(e.error));
+    };
 
-        return () => {
-            setResultFetch(null)
-        }
-    }, [resultFetch])
+    const onSpeechResults = (e) => {
+      //Invoked when SpeechRecognizer is finished recognizing
+      console.log('onSpeechResults: ', e);
+      setResults(e.value);
+    };
+
+    const onSpeechPartialResults = (e) => {
+      //Invoked when any results are computed
+      console.log('onSpeechPartialResults: ', e);
+      setPartialResults(e.value);
+    };
+
+    const onSpeechVolumeChanged = (e) => {
+      //Invoked when pitch that is recognized changed
+      console.log('onSpeechVolumeChanged: ', e);
+      setPitch(e.value);
+    };
+
+    const startRecognizing = async () => {
+      //Starts listening for speech for a specific locale
+      console.log('audio');
+      try {
+        await Voice.start('es-US');
+        setPitch('');
+        setError('');
+        setStarted('');
+        setResults([]);
+        setPartialResults([]);
+        setEnd('');
+      } catch (e) {
+        //eslint-disable-next-line
+        console.error(e);
+      }
+    };
+
+    const stopRecognizing = async () => {
+      //Stops listening for speech
+      try {
+        await Voice.stop();
+      } catch (e) {
+        //eslint-disable-next-line
+        console.error(e);
+      }
+    };
+
+    const cancelRecognizing = async () => {
+      //Cancels the speech recognition
+      try {
+        await Voice.cancel();
+      } catch (e) {
+        //eslint-disable-next-line
+        console.error(e);
+      }
+    };
+
+    const destroyRecognizer = async () => {
+      //Destroys the current SpeechRecognizer instance
+      try {
+        await Voice.destroy();
+        setPitch('');
+        setError('');
+        setStarted('');
+        setResults([]);
+        setPartialResults([]);
+        setEnd('');
+      } catch (e) {
+        //eslint-disable-next-line
+        console.error(e);
+      }
+    };
 
     return (
 
@@ -74,7 +133,7 @@ const HomeScreen = ({ navigation }) => {
             <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
 
                 <TouchableOpacity
-                    onPressIn={() => startRecording(setValue)} onPressOut={() => stopRecording(value, setResultFetch)}>
+                    onPressIn={async () => await startRecognizing()} onPressOut={async () => await stopRecognizing()}>
                     <Icon
                         name="microphone"
                         type='font-awesome'
