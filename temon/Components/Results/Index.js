a81015fb248695af8068405342291bd275de4da8
yzornetta <yzornetta@gmail.com>
2020-10-20 19:28:44 -0300

    Agrego lista de canciones

--- temon/Components/Results/Index.js
+++ temon/Components/Results/Index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {
-    Text, View,TouchableOpacity
-} from 'react-native';
+import {View, TouchableOpacity} from 'react-native';
+import ResultList from '../Results/ResultList';
 
 const ResultsScreen = ({navigation}) => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <TouchableOpacity onPress={()=>navigation.navigate('MusicPlayer')}>
-            <Text>Results</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <TouchableOpacity onPress={() => navigation.navigate('MusicPlayer')}>
+        <ResultList navigation={navigation}/>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 export default ResultsScreen;
