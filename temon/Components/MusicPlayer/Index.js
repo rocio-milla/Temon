d915117e44e61099a06e58b86cc7e6ff17447406
Leandro David Coronel <coronelleandro18@gmail.com>
2020-11-08 02:11:57 -1200

    se hicieron y probaron todos los gestos

--- temon/Components/MusicPlayer/Index.js
+++ temon/Components/MusicPlayer/Index.js
@@ -1,24 +1,29 @@
 import { useFocusEffect } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { Pressable, Text, TouchableOpacity, View } from 'react-native';
+import { Pressable, Text, TouchableOpacity,TouchableWithoutFeedback, View } from 'react-native';
 import { Icon, Slider } from 'react-native-elements';
-import TrackPlayer, { useTrackPlayerProgress } from 'react-native-track-player';
+import TrackPlayer from 'react-native-track-player';
+import {useTrackPlayerProgress} from 'react-native-track-player/lib/hooks';
 import runes from 'runes';
 import { PersonalConfig } from '../../PersonalConfig.js';
 import styles from './IndexStyle';
 
-const MusicPlayerScreen = ({ route }) => {
-  const { title, song } = route.params;
 
-  // const { position, duration } = useTrackPlayerProgress(100)
+const MusicPlayerScreen = ({ route }) => {
+  const { results,title, song } = route.params;
+
+  const { position, duration } = useTrackPlayerProgress(500)
   // const progress = TrackPlayer.useTrackPlayerProgress();
   const [buttonPlay, setButtonPlay] = useState("pause")
   const [idTrack, setIdTrack] = useState("")
   const [musicTheme, setMusicTheme] = useState({ id: "", url: "", title: "", duration: 0 })
+  const [count, setCount] = useState(0)
+  const [dateTouch,setDateTouch] = useState(null)
+  const [increment,setIncrement] = useState(0)
+  const [touchState,setTouchState] = useState({state1: 0,state2: 0})
   //const [count,setCount] = useState({value:0,state:0})
   //const [dateTouch,setDateTouch] = useState(null)
-  let date;
-  let count = 0;
+  
   let timer;
   let playerReady = false;
 
@@ -46,25 +51,176 @@ const MusicPlayerScreen = ({ route }) => {
 
         playerReady = true;
       }
+ 
+      var res = [];
+      res = results
+      console.log(res[0].video)
+
       await TrackPlayer.reset();
 
       console.log("Mi URL ", `${PersonalConfig.url}/musica/escuchar?url=${encodeURI(song)}`);
-
+      
+      /*for(let i=0;i<res.length;i++){
+        await TrackPlayer.add({
+          id: i,
+          url: `${PersonalConfig.url}/musica/escuchar?url=${encodeURI(res[i].url)}`,
+          title: res[i].video
+        });
+      }*/
+      
       await TrackPlayer.add({
         id: 1,
         url: `${PersonalConfig.url}/musica/escuchar?url=${encodeURI(song)}`,
         title: title
       });
+      
       await TrackPlayer.play();
       setMusicTheme({ ...musicTheme, title: title })
     };
-
+    setIncrement(null)
+    setDateTouch(null)
+    setCount(0)
+    setButtonPlay("pause")
+    setTouchState(0)
     setPlayer();
-
     return (() => {
       TrackPlayer.destroy();
     })
   }, [])
+  
+  
+
+  useEffect(()=>{
+    //hold touch
+    if(touchState==5){
+      let posi=0,incrementable=0; 
+      const interval = setInterval(() => {
+        incrementable+=1
+        if(increment!=null){
+          TrackPlayer.setVolume(0)
+          TrackPlayer.play()
+        }
+        if(increment == true){
+          posi = position + incrementable
+          console.log(posi); 
+          TrackPlayer.seekTo(posi)
+          //playAndStop()
+        }
+        else if(increment == false){
+          posi = position - incrementable
+          console.log(posi); 
+          TrackPlayer.seekTo(posi)
+          //playAndStop()
+        } 
+      }, 500);
+      return () => {
+        if(increment!=null){
+          clearInterval(interval)
+          TrackPlayer.seekTo(posi)
+          TrackPlayer.pause()
+          TrackPlayer.setVolume(1)
+          setIncrement(null)
+          TrackPlayer.getPosition().then((res)=>{console.log("pos-actual => "+ res)});
+          //TrackPlayer.play()
+        }else{
+          clearInterval(interval)   
+        }
+      };
+    }
+    //1 touch
+    const makeADifference = () => {    
+      let newDate = new Date();
+      let diff = dateTouch.getTime() - newDate.getTime();
+      let dateDiff = diff / 1000;
+      let diffSecond = Math.abs(dateDiff);
+      console.log("tiempo diff: "+diffSecond);
+      return diffSecond;
+    }
+
+    if(touchState==0){
+    }
+    else if(touchState==1){
+      const timer = setTimeout(() => {
+        playAndStop()
+        console.log("play")
+        setIncrement(null)
+      }, 900);
+      setCount(1)
+      setIncrement(true)
+      return () => clearTimeout(timer);
+    }
+    else if(touchState==2){
+      if(makeADifference()<=0.900){
+        const timer = setTimeout(() => {
+          nextTrack()
+          console.log("next 1")
+          setIncrement(null)
+        }, 900);
+        setCount(3)
+        setIncrement(false)
+        setDateTouch(new Date)
+        return () => clearTimeout(timer);
+      }
+      else{
+        const timer = setTimeout(() => {
+          console.log("play 1")
+          playAndStop()
+          setIncrement(null)
+        }, 900);
+        setCount(2)
+        setIncrement(true)
+        setDateTouch(new Date)
+        return () => clearTimeout(timer);
+      } 
+    }
+    else if(touchState==3){
+      if(makeADifference()<=0.900){
+        const timer = setTimeout(() => {
+          nextTrack()
+          console.log("next 2")
+          setIncrement(null)
+        }, 900)
+        setCount(3)
+        setIncrement(false)
+        setDateTouch(new Date)
+        return () => clearTimeout(timer);
+      }
+      else{
+        const timer = setTimeout(() => {
+          console.log("play 2")
+          playAndStop()
+          setIncrement(null)
+        }, 900)
+        setCount(1)
+        setIncrement(true)
+        setDateTouch(new Date)
+        return () => clearTimeout(timer);
+      }
+    }
+    else if(touchState==4){
+      if(makeADifference()<=0.900){
+        const timer = setTimeout(() => {
+          prevTrack()
+          console.log("prev")
+          setIncrement(null)
+        }, 900)
+        setCount(1)
+        setIncrement(false)
+        setDateTouch(new Date)
+        return () => clearTimeout(timer);
+      }
+      else{
+        const timer = setTimeout(() => {
+          playAndStop()
+          console.log("play prev 3 ")
+        }, 900)
+        setCount(1)
+        setIncrement(true)
+        setDateTouch(new Date)
+        return () => clearTimeout(timer);
+      }
+    }
+  },[touchState])
 
   useFocusEffect(
     React.useCallback(() => {
@@ -75,6 +231,7 @@ const MusicPlayerScreen = ({ route }) => {
 
   const playAndStop = () => {
     if (buttonPlay == "pause") {
+      TrackPlayer.setVolume(1)
       setButtonPlay("play")
       TrackPlayer.pause()
     }
@@ -92,8 +249,8 @@ const MusicPlayerScreen = ({ route }) => {
 
   const prevTrack = () => {
     console.log("previous track")
-    // TrackPlayer.skipToPrevious()
-    // TrackActual()
+    TrackPlayer.skipToPrevious()
+    TrackActual()
   }
 
   let updateTheme = (res) => {
@@ -111,9 +268,9 @@ const MusicPlayerScreen = ({ route }) => {
     })
   }
 
-  // const setPositionTrack = (e) => {
-  //   TrackPlayer.seekTo(e)
-  // }
+  const setPositionTrack = (e) => {
+    TrackPlayer.seekTo(e)
+  }
 
   const TrackActual = () => {
     TrackPlayer.getCurrentTrack().then(
@@ -122,43 +279,51 @@ const MusicPlayerScreen = ({ route }) => {
   }
 
   const touchScreen = () => {
-    if (count == 0) {
-      count += 1
-      date = new Date()
-      timer = setTimeout(function () { playAndStop() }, 100);
-      //console.log("cont init: "+count)
+    if(count == 0){
+      setDateTouch(new Date)
+      setTouchState(1)
     }
-    else {
-      let newDate = new Date()
-      let diff = date.getTime() - newDate.getTime()
-      let dateDiff = diff / 1000
-      let diffSecond = Math.abs(dateDiff)
-      if (diffSecond <= 1) {
-        count += 1
-        if (count == 2) {
-          clearTimeout(timer);
-          timer = setTimeout(function () { nextTrack() }, 100);
-        }
-
-        else if (count == 3) {
-          clearTimeout(timer);
-          timer = setTimeout(function () { prevTrack() }, 100);
-          count = 0
-        }
-        date = newDate
-      }
-      else {
-        count = 1
-        timer = setTimeout(function () { playAndStop() }, 100);
-        date = new Date()
-      }
+    else if(count == 1){
+      
+      setTouchState(2)
+    }
+    else if(count == 2){
+      setTouchState(3)
+    }
+    else if(count == 3){
+      setTouchState(4)
     }
   }
 
-  const longTouchScreen = () => {
-    console.log("toque largo");
+  let pressOutCancel = 0
+  let pressCancel = 0
+  
+  const _pressIn = (res) => {
+    setTouchState(5) 
   }
-
+  
+  const _pressOut = (res) => {
+    if(pressOutCancel==0){
+      setTouchState(6) 
+      pressCancel=1
+    }
+    else{
+      pressOutCancel=0
+    }
+  }
+  
+  const _press = (res) => {
+    if(pressCancel == 1){
+      pressCancel = 0
+    }
+    else{
+      setTouchState(6)
+      pressOutCancel = 1
+      console.log("onPress")
+      touchScreen()
+    }
+  }
+  
   return (
     <>
       <View style={styles.headers}>
@@ -172,7 +337,14 @@ const MusicPlayerScreen = ({ route }) => {
         </View>
       </View>
       <View style={styles.screen}>
-        <Pressable onLongPress={() => longTouchScreen()} style={styles.screenButton} onPress={() => { touchScreen() }} />
+        {/*onPress={() => { touchScreen() }} */}
+        <Pressable  
+          onPressIn={(res)=>{_pressIn()}}
+          onPressOut={(res)=>{_pressOut()}}
+          onPress={(res)=>{_press()}}
+          style={styles.screenButton}
+        >
+        </Pressable>
       </View>
       <View style={styles.viewName}>
         <View style={styles.name}>
@@ -192,18 +364,23 @@ const MusicPlayerScreen = ({ route }) => {
               <Icon name="forward" type='font-awesome' color='#9E00B4' reverse size={50} />
             </TouchableOpacity>
           </View>
-          {/* <Slider
+          <Text>{
+            /*position*/
+          }
+          </Text>
+          {/*<Slider
             style={styles.slider}
             minimumValue={0}
             step={1}
             maximumValue={duration}
             value={position}
-            // onValueChange={(res) => setPositionTrack(res)}
+            animationType="timing"
+            onValueChange={(res) => setPositionTrack(res)}
             minimumTrackTintColor="#000"
             maximumTrackTintColor="#C3C3C3"
             trackStyle={{ height: 30, backgroundColor: 'transparent' }}
             thumbStyle={{ height: 20, width: 20, backgroundColor: 'transparent' }}
-          /> */}
+          />*/}
         </View>
       </View>
     </>
