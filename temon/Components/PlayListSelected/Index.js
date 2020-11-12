e9cb4627918b79cd964f227742b0e2bc49665971
mquiroz25 <mquiroz2535@gmail.com>
2020-11-12 13:18:08 -0300

    se muestra header con nombre y color de playlist elegido

--- temon/Components/PlayListSelected/Index.js
+++ temon/Components/PlayListSelected/Index.js
@@ -1,64 +1,67 @@
-import { size } from 'lodash';
-import React, { useState, useCallback } from "react";
-import { ActivityIndicator, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Divider, Button } from 'react-native-elements';
-import runes from 'runes';
+import React, { useEffect} from "react";
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import {useNavigation } from '@react-navigation/native';
-import { color } from 'react-native-reanimated';
 
-var SQLite = require('react-native-sqlite-storage');
-var db = SQLite.openDatabase({name: 'test.db', createFromLocation: '~sqliteexample.db'});
+  const ScreenPlayListSelected = ( {route }) => {
 
-
-
-const ScreenPlayListSelected = ( {route }) => {
-
+  const navigation = useNavigation() 
   const { title, colour,songs} = route.params;
-/*  console.log("ffff");
-  db.transaction(tx => {
-
-    tx.executeSql('SELECT * from playlist', [], (tx, results) => {
-       var len = results.rows.length;
-
-       let elements = [];
-
-         for (let i = 0; i < len; i++) {
-           elements.push(results.rows.item(i));
-         }
-         this.setState({ elementList:elements});
-         console.log(this.state.elementList)
-     });
-
-  });*/
 
 
-  //onst { navigation, route } = props;
+  useEffect(() => {
+    navigation.setOptions({ title: title,            headerStyle: {
+      backgroundColor: colour,
+      height: 100,
+    },})
 
-  //console.log(route.params);
+    }, []);
 
   return (
-
     <>
          <View>
-         <Text style={{fontSize :38, backgroundColor:colour}}>{title}</Text>
+
         <FlatList
-                  data={songs}
+
+              data={songs}
                 
-                  keyExtractor={(item, index) => index.toString()}
-                  renderItem={({ item }) => (
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ item }) => (
 
-                    <View key={item.id} style={{ backgroundColor: 'white' ,flexDirection: 'row'  ,alignItems:'center'  ,  justifyContent: 'center' }}>
+                <View key={item.id} style={styles.cancionView}>
 
-                      <Text style={{fontSize :20}}>{item.title}</Text>
+                  <Text style={styles.cancionName}>{item.title}</Text>
+                  <Text style={styles.divider}></Text>
 
-                    </View>
+                </View>
                   )}
                 />
-            
-  </View>
+          </View>
     </>
 );
 }
 export default ScreenPlayListSelected;
 
 
+const styles = StyleSheet.create({
+
+
+  cancionView: {
+    backgroundColor: 'white' ,flexDirection: 'column'  ,alignItems:'center'  ,  justifyContent: 'center' ,   marginTop: 8,
+    marginBottom: 8,
+    marginLeft: 20,
+    marginRight: 20
+  },
+  cancionName: {
+    fontSize: 38,
+    fontWeight: "bold",
+    marginTop: 4,
+  },
+  divider: {
+    backgroundColor: "#4f831f",
+    marginLeft: 20,
+    marginRight: 20,
+    height: 12,
+    width:320
+  },
+
+});
